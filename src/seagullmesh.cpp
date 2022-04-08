503b4ef11b0ff11874fcea24813f5061d7132c1e
Darik Gamble <darik.gamble@gmail.com>
2022-04-08 08:35:26 -0400

    that fixed it, now try properties

--- src/seagullmesh.cpp
+++ src/seagullmesh.cpp
@@ -2,9 +2,9 @@
 namespace py = pybind11;
 
 //void init_mesh(py::module&);
-// void init_properties(py::module&);
+void init_properties(py::module&);
 //void init_corefine(py::module&);
-void init_meshing(py::module&);
+// void init_meshing(py::module&);
 //void init_locate(py::module&);
 //void init_parametrize(py::module&);
 //void init_triangulate(py::module&);
@@ -12,9 +12,9 @@ void init_meshing(py::module&);
 PYBIND11_MODULE(_seagullmesh, m) {
     m.doc() = "";
     // init_mesh(m);
-    // init_properties(m);
+    init_properties(m);
     // init_corefine(m);
-    init_meshing(m);
+    // init_meshing(m);
     // init_locate(m);
     // init_parametrize(m);
     // init_triangulate(m);
